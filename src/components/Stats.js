2037bdca3cc1f2a2de1b1a9150c1edfc7f4e74eb
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-03-24 22:58:42 +0100

    table..

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { payments } from '../constants/mock';
 import '../assets/styles/components/stats.scss';
 
 export class Stats extends Component {
@@ -118,6 +119,94 @@ export class Stats extends Component {
               </div>
             </div>
           </div>
+        </section>
+        <section className="payments">
+          <h2 className="title">Payments</h2>
+          <div className="filter-format">
+            <div className="filter-number">
+              <span>Showing</span>
+              <select>
+                <option value="20">20</option>
+                <option value="40">40</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
+              <span>out of 500 payments</span>
+            </div>
+            <div className="search">
+              <img src={require('../assets/images/search.png')} alt="*" />
+              <input type="search" placeholder="Search payments" className="search-payments" />
+            </div>
+            <div className="filter-type">
+              <span>Showing</span>
+              <select>
+                <option value="All">All</option>
+                <option value="Reconcilled">Reconcilled</option>
+                <option value="Un-reconcilled">Un-reconcilled</option>
+                <option value="Settled">Settled</option>
+                <option value="Unsettled">Unsettled</option>
+              </select>
+            </div>
+          </div>
+          <div className="payments-list">
+            <table className="list-table">
+              <thead>
+                <tr>
+                  <th>Item Type</th>
+                  <th>Price</th>
+                  <th>Transaction No</th>
+                  <th>Time</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {payments.map((payment, index) => (
+                  <tr className="each-row">
+                    <td className="type" key={index}>
+                      <img src={require('../assets/images/payment.png')} alt="" />
+                      <span>{payment.item}</span>
+                    </td>
+                    <td>{payment.price}</td>
+                    <td>{payment.transactionNo}</td>
+                    <td>{payment.time}</td>
+                    <td className="action">
+                      <button className={`status ${payment.status}`}>
+                        <span className={`${payment.status}`}></span>
+                        <span className="tile">{payment.status}</span>
+                      </button>
+                      <img src={require('../assets/images/down-arrow.png')} alt="" />
+                    </td>
+                  </tr>
+                ))}
+                <tr className="each-row">
+                  <td className="type">
+                    <img src={require('../assets/images/payment.png')} alt="" />
+                    <span>Apple Mac Book 15” 250 SSD 12GB</span>
+                  </td>
+                  <td>$73430</td>
+                  <td>12345687437890</td>
+                  <td>12:30</td>
+                  <td className="action">
+                    <button className="status pending">
+                      <span className="pending"></span>
+                      <span className="tile">Uncontrolled</span>
+                    </button>
+                    <img src={require('../assets/images/down-arrow.png')} alt="" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="navigation">
+            <div className="stat">Showing 1 to 10 of 500 entries</div>
+            <div className="navs">
+              <button>Previous</button>
+              <button className="active">1</button>
+              <button>2</button>
+              <button>Next</button>
+            </div>
+          </div>
+
         </section>
       </div>
     )
---
+++ src/constants/mock.js
@@ -0,0 +1,68 @@
+const payments = [
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'reconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'pending'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'reconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'reconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'pending'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'unreconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'reconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'unreconciled'
+  },
+  {
+    item: 'Apple Mac Book 15” 250 SSD 12GB',
+    price: '$73430',
+    transactionNo: '1234567890',
+    time: '12:30',
+    status: 'pending'
+  },
+]
+
+
+export { payments };
